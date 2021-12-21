7e5e3b1d9440666cc569fac1d19ec7ea2e36f658
태구슝슝 <eblim3@gmail.com>
2021-12-21 21:38:56 +0900

    SCOPE

---
+++ Day03/src/Scope.java
@@ -0,0 +1,16 @@
+
+public class Scope {
+
+	statc String name = "임은비 바보";
+	
+	
+	public static void main(String args[]) {
+		String name = "임은비";
+		System.out.println(name);
+        printN		 
+		System.out.println(name);
+		
+	
+        // 책 45P
+        
+
