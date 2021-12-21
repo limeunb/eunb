ffbd106dce6bee2cb3b3263f6f044bd4915964ce
태구슝슝 <eblim3@gmail.com>
2021-12-21 21:22:05 +0900

    리터널

---
+++ Day03/src/LiteralExample.java
@@ -0,0 +1,15 @@
+
+public class LiteralExample {
+
+	public static void main(String[] arge) {
+		
+		
+		//-- 리터널(이스케이프 시퀀스)
+		String desc =" \"임은비\"\n 술\n한잔 \t하자";
+		System.out.println(desc);
+		
+		
+		// 임은비 술 한잔하자 
+	}
+}
+    //\r\n
