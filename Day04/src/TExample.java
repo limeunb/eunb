a837b4e6bcf1b213a3bc2c75025bccee54fd9aa8
태구슝슝 <eblim3@gmail.com>
2021-12-23 21:40:25 +0900

    TExample

---
+++ Day04/src/TExample.java
@@ -0,0 +1,20 @@
+// 삼항연산자
+public class TExample {
+
+	 public static void main(String[] args) {
+		 
+		 //1. 항은 3개
+		 //(비교문) ? 비교문이 참이면 첫번째: 비교문이 거짓이면 두번째;
+		 
+		 int a =10, b=20;
+		 int c = (a>b)?100:-100;
+		 System.out.println(c);
+	    // 위 답은 비교문이 거짓이므로 - 100
+		 
+		 
+		 System.out.println
+		 
+	 }
+	
+	
+}
