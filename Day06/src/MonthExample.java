b64ae4312a308fb927fda303e2573f16992206ac
태구슝슝 <eblim3@gmail.com>
2021-12-28 22:04:31 +0900

    MonthExample

---
+++ Day06/src/MonthExample.java
@@ -0,0 +1,31 @@
+
+public class MonthExample {
+
+ public static void main(String args[]) {
+	 
+  if(args.length == 0) {
+	  System.out.println("usag: java MonthExample{month}");
+  } else {
+	  String[] arrMonthName = {
+			  
+			  "Jan",
+			  "Feb",
+			  "Mar",
+			  "Apr",
+			  "May",
+			  "June",
+			  "July",
+			  "Aug",
+			  "Sept",
+			  "Oct",
+			  "Nov",
+			  "Dec",
+	  };
+	  int command = Integer.parseInt(args[0]) -1;
+	  System.out.println(arrMonthName[command]);
+	  
+  }
+
+ }
+ 
+}
