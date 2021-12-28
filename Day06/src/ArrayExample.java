cf26351e66e4f93783099fa11df8d937a0fc78a1
태구슝슝 <eblim3@gmail.com>
2021-12-28 22:06:32 +0900

    Array

--- Day06/src/ArrayExample.java
+++ Day06/src/ArrayExample.java
@@ -29,9 +29,43 @@ public class ArrayExample {
 		int[]b= {1,2,3,4,5};
 		for(int i=0; i < b.length; i++) {
 			System.out.println(i+1);
+		}
+	
+	         //배열정리
+			//1. 같은 데이터 타입을 가지는 연속적인 방
+		   // int[5] → 4*5 = 20bytes
 			
+			// 배열 선언법 
+			// int[] 변수명;   < 준비! (아직 시작은 안했음)
+		    // char[] 변수명;  
+			
+			// 배열 선언과 동시에 방 만들기
+		    // char[] 변수명 = new char[5];
+			// 값넣기a : a[0] = 97; a[1]=98............
+			// 값넣기b : for문 같은 반복문 사용하는 것
 		
+			char[] alpha = new char[26];
+			
+			char alpha_start = 'A';
+			
+			for(int i =0; i < alpha.length; i++) {
+				alpha[i] = alpha_start ++;
+			}
+			for(int i =0; i < alpha.length; i++){
+			System.out.println(alpha[i]);
 		}
 		
+			//-- 배열 선언과 동시에 값 때려 넣기
+			// 데이터 타입 배열변수명 ={값};
+			 
+			char[]arrCh= {97,98,99};
+			
+			// 	char[]arrCh= {97,98,99}; 97,98,99 등 하나하나 쳐야 한다는 번거로움이 있음 위에 방식이 간편 
+			
+			
+			
+			
+			
+			
 	}
 }
