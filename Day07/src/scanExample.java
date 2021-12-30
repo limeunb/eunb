342f83efd55ce76c75626d100b8dd41b30904163
태구슝슝 <eblim3@gmail.com>
2021-12-30 22:01:14 +0900

    public class scanExample

---
+++ Day07/src/scanExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class scanExample {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문자열을 입력해주세요>");
+		String input = sc. nextLine();
+		
+		System.out.println("입력문자열 :"+input);
+		
+	}
+	
+}
