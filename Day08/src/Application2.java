e78f065955b0e2ed378db6b5c9d4fd022185ec31
태구슝슝 <eblim3@gmail.com>
2022-01-04 20:35:02 +0900

    Application

---
+++ Day08/src/Application2.java
@@ -0,0 +1,47 @@
+
+public class Application2 {
+ 
+	public static void main(String[] args) {
+//	 Korean K = new Korean();
+ //    K.name = "나는 한국인";
+  //   K.sayHello();
+    
+		
+		Person p = new Janpanese();
+		p.name = "나까무라";
+		p.sayHello();
+	}
+
+// class 문법
+//[접근한정자] class 클래스명 {//내용}
+
+class Person{
+
+String name; // 속성, 필드 
+
+public void sayHello() {
+	System.out.println(name+"입니다. 안녕하세요.");
+}
+}
+
+// 상속은 extends [Class명]
+ class Korean extends Person{
+  public void sayHello() {
+	  System.out.println(name+"입니다. 안녕하세요");
+  }
+}
+
+class Janpanese extends Person{
+public void sayHello() { 
+ System.out.println(name+"입니다. 안녕하세요");
+}
+}
+
+class Chinese extends Person{
+public void sayHello() { 
+	 System.out.println(name+"입니다. 안녕하세요");
+
+}
+}
+}
+
---
+++ Day08/src/Application3.java
@@ -0,0 +1,46 @@
+
+public class Application3 {
+
+	public static void main (String args[]) {
+		// -- 객체를 생성하는 방법
+		
+		MathExample math = new MathExample();
+	    int sum = math.plus(10,20);
+	    System.out.println(sum);
+	}
+}
+
+
+//-- 1. 클래스 정의
+ 
+class MathExample {
+	
+	// f(x,y) = (x+y)
+	
+	 //ㅡㅡㅡ속성(필드)ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 
+	//String name;
+	//int age;
+	//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	
+	//-- 메소드들 (행위)
+	//-- 메소드 문법
+	//-- [접근한정자] 리턴데이터타입 메소드명 (매게변수){
+	// 행동문단
+	//}
+	
+	//void sayHello(String name) {
+		//System.out.println(name);
+	
+	// void 리턴이 없는 메소드 
+	void printName() {
+		System.out.println("안녕");
+	}
+	
+	int plus(int x, int y) {
+		return x +y;
+	}
+	 int minus(int x, int y) {
+		 return x-y;
+		
+	}
+}
